1e73d94ffc1106eb7e1d6c2eb1d48f01b94ed0d6
Manfred Baumstark <loc-maba@msm-nbs-maba.ad.uniklinik-freiburg.de>
2020-04-11 12:52:13 +0200

    Work in process. *** Do not run ***

--- global.R
+++ global.R
@@ -4,9 +4,10 @@ library(readr)
 library(lubridate)
 library(reshape2)
 library(ggplot2)
-library("jsonlite")
+library(jsonlite)
+library(dplyr)
 
-#compare with NAs
+# compare with NAs
 compareNA <- function(v1,v2) {
   same <- (v1 == v2) | (is.na(v1) & is.na(v2))
   same[is.na(same)] <- FALSE
@@ -24,6 +25,13 @@ compareGE <- function(v1,v2) {
   same[is.na(same)] <- FALSE
   return(same)
 }
+
+##### type of cases ####
+ctype <- c("Confirmed Cases" , 
+           "Deaths", 
+           "Recovered Cases",
+           "Active Cases" 
+)
 
 # Johns Hopkins Master Repository #############################
 # from datahub.io (more simple repository)
@@ -63,66 +71,12 @@ tm$Delta_Deaths[2:length(tm$Delta_Deaths)] <- diff(tm$Deaths, 1)
 tm$Delta_Deaths[! tm$cmatch] <- tm$Deaths[! tm$cmatch]
 
 tm$Rate_Confirmed <- (tm$Delta_Confirmed / tm$Confirmed) * 100
-# tm$Rate_Confirmed[compareLE(tm$Rate_Confirmed, 0)] <- NA
 tm$Rate_Recovered <- (tm$Delta_Recovered / tm$Recovered) * 100
-# tm$Rate_Recovered[compareLE(tm$Rate_Recovered, 0)] <- NA
 tm$Rate_Active <- (tm$Delta_Active / tm$Active) * 100
-# tm$Rate_Active[compareLE(tm$Rate_Active, 0)] <- NA
 tm$Rate_Deaths <- (tm$Delta_Deaths / tm$Deaths) * 100
-# tm$Rate_Deaths[compareLE(tm$Rate_Deaths, 0)] <- NA
 
-# ECDC repository #########################
-url <- "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv" 
 
-ecdc_raw <- read_csv(url)
-ecdc_raw$dateRep <- dmy(ecdc_raw$dateRep)
-
-#rename countries to match Johns Hopkins
-ecdc_raw$countriesAndTerritories <- gsub("_", " ", ecdc_raw$countriesAndTerritories, fixed = TRUE)
-ecdc_raw$countriesAndTerritories[ecdc_raw$countriesAndTerritories == "South Korea"] <- "Korea, South"
-ecdc_raw$countriesAndTerritories[ecdc_raw$countriesAndTerritories == "United States of America"] <- "US"
-ecdc_raw$countriesAndTerritories[ecdc_raw$countriesAndTerritories == "Czech Republic"] <- "Czechia"
-ecdc_raw$popData2018[ecdc_raw$countriesAndTerritories == "Czechia"] <- 10650000 # currently missing 
-
-ecdc <- subset(ecdc_raw, select = c("dateRep", "cases", "deaths", "countriesAndTerritories"))
-names(ecdc) <- c("Date", "Delta_Confirmed", "Delta_Deaths", "Country_Region")
-
-ecdc <- ecdc[order(ecdc$Country_Region, ecdc$Date),]
-
-ecdc$Confirmed <- 0
-ecdc$Confirmed[1] <- ecdc$Delta_Confirmed[1]
-
-for (i in 2:length(ecdc$Confirmed)) {
-  ecdc$Confirmed[i] = ecdc$Confirmed[i-1] + ecdc$Delta_Confirmed[i]
-  if (ecdc$Country_Region[i] != ecdc$Country_Region[i-1]){
-    ecdc$Confirmed[i] = ecdc$Delta_Confirmed[i]
-  }
-}
-
-ecdc$Deaths <- 0
-ecdc$Deaths[1] <- ecdc$Delta_Deaths[1]
-
-for (i in 2:length(ecdc$Deaths)) {
-  ecdc$Deaths[i] = ecdc$Deaths[i-1] + ecdc$Delta_Deaths[i]
-  if (ecdc$Country_Region[i] != ecdc$Country_Region[i-1]){
-    ecdc$Deaths[i] = ecdc$Delta_Deaths[i]
-  }
-}
-
-ecdc$Recovered <- 0
-ecdc$Active <- 0
-ecdc$Delta_Recovered <- 0
-ecdc$Delta_Active <- 0
-ecdc$Rate_Recovered <- 0
-ecdc$Rate_Active <- 0
-
-ecdc$Rate_Confirmed <- (ecdc$Delta_Confirmed / ecdc$Confirmed) * 100
-# ecdc$Rate_Confirmed[compareLE(ecdc$Rate_Confirmed, 0)] <- NA
-ecdc$Rate_Deaths <- (ecdc$Delta_Deaths / ecdc$Deaths) * 100
-# ecdc$Rate_Deaths[compareLE(ecdc$Rate_Deaths, 0)] <- NA
-
-# RKI Data
-# warning("vor RKI Daten") #DEBUG
+#### RKI Data
 rki_data <- "https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv"
 rki_full <- read_delim(rki_data, ",")
 
@@ -188,7 +142,7 @@ rkia$Rate_Deaths <- (rkia$Delta_Deaths / rkia$Deaths) * 100
 # rkia$Rate_Deaths[compareLE(rkia$Rate_Deaths, 0)] <- NA
 
 rkig <- aggregate(cbind(Delta_Confirmed, Delta_Deaths, Delta_Recovered, Delta_Active) ~ Date, rkia, sum)
-rkig$Country_Region <- "Germany"
+rkig$Country_Region <- "Germany (RKI)"
 
 rkig$Confirmed <- 0
 rkig$Confirmed[1] <- rkig$Delta_Confirmed[1]
@@ -231,86 +185,111 @@ for (i in 2:length(rkig$Active)) {
 }
 
 rkig$Rate_Confirmed <- (rkig$Delta_Confirmed / rkig$Confirmed) * 100
-# rkig$Rate_Confirmed[compareLE(rkig$Rate_Confirmed, 0)] <- NA
 rkig$Rate_Deaths <- (rkig$Delta_Deaths / rkig$Deaths) * 100
-# rkig$Rate_Deaths[compareLE(rkig$Rate_Deaths, 0)] <- NA
 rkig$Rate_Recovered <- (rkig$Delta_Recovered / rkig$Recovered) * 100
 rkig$Rate_Active <- (rkig$Delta_Active / rkig$Active) * 100
 
 max_date <- max(max(tm$Date), max(ecdc$Date), max(rkig$Date))
 min_date <- min(min(tm$Date), min(ecdc$Date), min(rkig$Date))
 
-######################################################################
-ctype <- c("Confirmed Cases" , 
-           "Deaths", 
-           "Recovered Cases",
-           "Active Cases" 
-)
+#### Total data set 
 
-# Wichtige Länder (cases > 1000) und deren Einwohnerzahlen
+all <- bind_rows(tm, rkia, rkig) 
+
+#### countries with N cases > 1000 and population 
 
 tmc <- as.data.frame(unique(subset(tm, Confirmed > 1000)$Country_Region) )
 names(tmc)[1] <- "Country_Region"
 
-ecdcc <- as.data.frame(unique(subset(ecdc, Confirmed > 1000)$Country_Region) )
-names(ecdcc)[1] <- "Country_Region"
+# ECDC repository #### only for Population ####
+url <- "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv" 
+
+ecdc_raw <- read_csv(url)
+ecdc_raw$dateRep <- dmy(ecdc_raw$dateRep)
+
+#rename countries to match Johns Hopkins
+ecdc_raw$countriesAndTerritories <- gsub("_", " ", ecdc_raw$countriesAndTerritories, fixed = TRUE)
+ecdc_raw$countriesAndTerritories[ecdc_raw$countriesAndTerritories == "South Korea"] <- "Korea, South"
+ecdc_raw$countriesAndTerritories[ecdc_raw$countriesAndTerritories == "United States of America"] <- "US"
+ecdc_raw$countriesAndTerritories[ecdc_raw$countriesAndTerritories == "Czech Republic"] <- "Czechia"
+ecdc_raw$popData2018[ecdc_raw$countriesAndTerritories == "Czechia"] <- 10650000 # currently missing 
 
 population <- aggregate(popData2018 ~ countriesAndTerritories, 
                         subset(ecdc_raw, !is.na(popData2018)), mean )
 
-# check_sd <- aggregate(popData2018 ~ countriesAndTerritories, #DEBUG
-#                       subset(ecdc_raw, !is.na(popData2018)), sd) #DEBUG
+# check_sd <- aggregate(popData2018 ~ countriesAndTerritories,      #DEBUG
+#                       subset(ecdc_raw, !is.na(popData2018)), sd)  #DEBUG
 
 names(population) <- c("Country_Region", "Population")
 
 population$Population <- population$Population / 1000000
 
-pc <- as.data.frame(unique(population$Country_Region) )
-names(pc)[1] <- "Country_Region"
+cp <- merge(tmc, population, by = "Country_Region", all.x = TRUE)
 
-ctr2 <- merge(ecdcc, population, by = "Country_Region", all.x = TRUE)
-
-cp <- merge(tmc, ctr2, by = "Country_Region", all = TRUE)
 
 print(cp$Country_Region[is.na(cp$Population)]) # DEBUG
 
 cp <- subset(cp, ! is.na(cp$Population))
+cp$Country_Region <- as.character(cp$Country_Region)
+
+grki <- data.frame("Germany (RKI)",	cp$Population[cp$Country_Region == "Germany"], stringsAsFactors = FALSE)
+names(grki) <- names(cp)  
+
+cp <- bind_rows(cp, grki)
+cp$Country_Region <- factor(cp$Country_Region)
 
 countries <- as.character(cp$Country_Region)
 g_inh <- array(0, dim = length(countries), dimnames = list(countries))
 g_inh[countries] <- cp$Population
 
-##########
+####
 rki_countries <- as.character(unique(rkia$Country_Region))
-rki_inh <- array(0, dim = length(rki_countries), dimnames = list(rki_countries))
-rki_inh["Baden-Württemberg"] <- 11.07 
-rki_inh["Bayern"] <- 13.08
-rki_inh["Berlin"] <- 3.6
-rki_inh["Brandenburg"] <- 2.52
-rki_inh["Bremen"] <- 0.68
-rki_inh["Hamburg"] <- 1.84
-rki_inh["Hessen"] <- 6.27
-rki_inh["Mecklenburg-Vorpommern"] <- 1.61
-rki_inh["Niedersachsen"] <- 7.98
-rki_inh["Nordrhein-Westfalen"] <- 17.93
-rki_inh["Rheinland-Pfalz"] <- 4.09
-rki_inh["Saarland"] <- 0.99
-rki_inh["Sachsen"] <- 4.08
-rki_inh["Sachsen-Anhalt"] <- 2.21
-rki_inh["Schleswig-Holstein"] <- 2.14
-rki_inh["Thüringen"] <- 2.14
-rki_inh["Germany"] <- 82.79
+
+Einwohner <- read_delim("12411-0013-editiert.csv", 
+                        ";", escape_double = FALSE, locale = locale(encoding = "WINDOWS-1252"), 
+                        trim_ws = TRUE)
+
+x <- melt(Einwohner, 
+          id.vars = "Alter", 
+          value.name = "Population",
+          variable.name = "Country_Region")
+x$Country_Region <- as.character(x$Country_Region)
+x$sex <- substr(x$Country_Region, nchar(x$Country_Region), nchar(x$Country_Region))
+x$Country_Region <- gsub("_.", "", x$Country_Region)
+
+sum(x$Population) / 1000000
+
+# rki_inh <- array(0, dim = length(rki_countries), dimnames = list(rki_countries))
+# rki_inh["Baden-Württemberg"] <- 11.07 
+# rki_inh["Bayern"] <- 13.08
+# rki_inh["Berlin"] <- 3.6
+# rki_inh["Brandenburg"] <- 2.52
+# rki_inh["Bremen"] <- 0.68
+# rki_inh["Hamburg"] <- 1.84
+# rki_inh["Hessen"] <- 6.27
+# rki_inh["Mecklenburg-Vorpommern"] <- 1.61
+# rki_inh["Niedersachsen"] <- 7.98
+# rki_inh["Nordrhein-Westfalen"] <- 17.93
+# rki_inh["Rheinland-Pfalz"] <- 4.09
+# rki_inh["Saarland"] <- 0.99
+# rki_inh["Sachsen"] <- 4.08
+# rki_inh["Sachsen-Anhalt"] <- 2.21
+# rki_inh["Schleswig-Holstein"] <- 2.14
+# rki_inh["Thüringen"] <- 2.14
+# rki_inh["Germany"] <- 82.79
 
 closeAllConnections()
 
 
-###################### ToDo
+#### Infos
 # https://stats.idre.ucla.edu/r/faq/how-can-i-explore-different-smooths-in-ggplot2/
 
-###################### github:
+#### github
 # https://happygitwithr.com/existing-github-first.html
 
+#### tanh fit
 #https://www.reddit.com/r/dataisbeautiful/comments/fs9tj2/oc_fit_tanh_to_covid19_data_provided_by_john/fm0731j/
 #(a0 * (tanh((x + a1) * a2) + 1))
 
+#### Bugs
 # Crash if no country selected
